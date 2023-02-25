5a70741db9d2b5b8b4572985bc9bc019c91af5ae
Kajal Pourjalil <karastar771@gmail.com>
2023-02-25 23:35:57 +0300

    complete categories and items added

--- React-menu-app/src/Components/Items/Items.jsx
+++ React-menu-app/src/Components/Items/Items.jsx
@@ -1,13 +1,167 @@
 import React from "react";
-import Items from '../Items/Items.css'
+import "../Items/Items.css";
 
-
-class Items extends React.Component{
-    render() {
-        return (
-            <div></div>
-        )
-    }
+class Items extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>SIGNATURE COFFEES</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Americano</h3>
+          <h3>Affogato</h3>
+          <h3>Brewed Coffee</h3>
+          <h3>Espresso</h3>
+          <h3>Cappuccino</h3>
+          <h3>CafeLatte</h3>
+          <h3>Caffe Macchiato</h3>
+          <h3>French Press</h3>
+          <h3>Turkish Coffee</h3>
+        </div>
+        <div>
+          <p>
+            <i>
+              Add a flavour: Nutella, Vanilla, Mint, Caramel, Hazelnut, Irish
+            </i>
+          </p>
+        </div>
+        <div>
+          <h2>ON THE ROCKS</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Iced Americano</h3>
+          <h3>Iced Latte</h3>
+          <h3>Iced Mocha</h3>
+          <h3>Iced Cappuccino</h3>
+          <h3>Iced Caramel Macchiato</h3>
+        </div>
+        <div>
+          <h2>CBD SPECIALS</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Cold Spanish Latte</h3>
+          <h3>Hot Spanish Latte</h3>
+          <h3>Diamond Cappuccino</h3>
+          <h3>Gold Cappuccino</h3>
+          <h3>Signature Affogato</h3>
+          <h3>Coffee Tonic</h3>
+          <h3>Pearl Drop Cappuccino</h3>
+          <h3>Sea Salt Iced Americano</h3>
+          <h3>Rose Cappuccino</h3>
+        </div>
+        <div>
+          <h2>FRAPPES</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Classic Cold Coffee</h3>
+          <h3>Mocha Freeze</h3>
+          <h3>Chocolate Explosion</h3>
+          <h3>Rose Cardamom Frappe</h3>
+          <h3>Strawberry Frappe</h3>
+          <h3>Peanut Butter</h3>
+          <h3>Oreo Frappe</h3>
+          <h3>Vanilla Frappe</h3>
+        </div>
+        <div>
+          <h2>SMOOTHIES</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Blueberry Granola Smoothie</h3>
+          <h3>Strawberry Banana Smoothie</h3>
+          <h3>Cocoa, Granola and Soy</h3>
+          <h3>Milk Smoothie</h3>
+          <h3>Mojito</h3>
+        </div>
+        <div>
+          <h2>HOT CHOCOLATES</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Nutella Hot Chocolate</h3>
+          <h3>Peanut Butter Hot Chocolate</h3>
+          <h3>Traditional Hot Chocolate</h3>
+        </div>
+        <div>
+          <p>
+            <i>
+              Add a flavour: Nutella, Vanilla, Mint, Caramel, Hazelnut, Irish
+            </i>
+          </p>
+        </div>
+        <div>
+          <h2>PREMIUM MILKSHAKES</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Red Velvet</h3>
+          <h3>Ferrero Rocher</h3>
+          <h3>Snickers</h3>
+          <h3>Twix</h3>
+          <h3>Kit Kat</h3>
+          <h3>Chocolate Peppermint</h3>
+          <h3>Banana Caramel</h3>
+          <h3>Blueberry</h3>
+        </div>
+        <div>
+          <h2>FREAKSHAKES</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Melbourne</h3>
+          <h3>Childhood Wonderland</h3>
+          <h3>Gold Coast Pretzel</h3>
+        </div>
+        <div>
+          <h2>LUXURY LEAF TEAS</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Strawberry Black Tea</h3>
+          <h3>Earl Grey Tea</h3>
+          <h3>White Berry Tea</h3>
+          <h3>Kashmiri Kahwa Tea</h3>
+          <h3>Jasmine Tea</h3>
+          <h3>Green Tea</h3>
+          <h3>Peppermint Tea</h3>
+          <h3>Peach Tea</h3>
+          <h3>Apple Tea</h3>
+        </div>
+        <div>
+          <h2>ICED TEAS</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Lemon Iced Tea</h3>
+          <h3>Passion Fruit Iced Tea</h3>
+          <h3>Strawberry Iced</h3>
+          <h3>Lavender Iced Tea</h3>
+          <h3>Peach Iced Tea</h3>
+        </div>
+        <div>
+          <h2>CHAI</h2>
+        </div>
+        <img />
+        <div>
+          <h3>Traditional Chai</h3>
+          <h3>Masala Chai</h3>
+          <h3>Ginger Chai</h3>
+          <h3>Elaichi Chai</h3>
+          <h3>Gulkand Chai</h3>
+        </div>
+        <div>
+          <p>
+            <i>Replace regular milk with Soy milk or Skimmed milk</i>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Items
+export default Items;
