4de1b842c10b13a2b73802ca1fd00a5dc182c183
Kajal Pourjalil <karastar771@gmail.com>
2023-02-25 13:56:33 +0300

    Components

---
+++ React-menu-app/src/Components/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Menu from '../Menu/Menu'
+import Categories from '../Categories/Caregories'
+import Items from '../Items/Items'
+import Name from '../Name/Name'
+import Prices from '../Prices/Prices'
+import Socials from '../Socials/Socials'
+
+import './App.css'
+
+function App() {
+  // return ()
+}
+
+export default App
---
+++ React-menu-app/src/Components/Menu/Menu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import '../Menu/Menu.css'
+
+class Menu extends React.Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+    
+
+
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default Menu
